57c213a2a40b482d368bfb8077110fad2fdd167d
Grady Martin <Gr@dyMart.in>
2023-01-19 01:14:24 -0600

    doc building suffix bug
    The default file extension was incorrectly overwritten for some reason.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,6 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.md'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
