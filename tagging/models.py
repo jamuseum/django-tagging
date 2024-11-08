e06a01f1857b76f4c87c41b3a66e4d6144944f99
Grady <GMartin@janm.org>
2024-11-08 06:24:54 -0600

    django 5
    https://docs.djangoproject.com/en/5.1/ref/exceptions/#django.core.exceptions.FullResultSet

--- tagging/models.py
+++ tagging/models.py
@@ -3,6 +3,7 @@ Models and managers for tagging.
 """
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import FullResultSet
 from django.db import connection
 from django.db import models
 from django.db.models.query_utils import Q
@@ -177,7 +178,10 @@ class TagManager(models.Manager):
         Passing a value for ``min_count`` implies ``counts=True``.
         """
         compiler = queryset.query.get_compiler(using=queryset.db)
-        where, params = compiler.compile(queryset.query.where)
+        try:
+            where, params = compiler.compile(queryset.query.where)
+        except FullResultSet:
+            where, params = "", []
         extra_joins = ' '.join(compiler.get_from_clause()[0][1:])
 
         if where:
